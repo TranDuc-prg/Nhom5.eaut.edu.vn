cf5dea6dddc5c196c152e9a758a53726ad3b6317
TranDuc-prg <tduc45310@gmail.com>
2026-04-12 00:10:54 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,23 @@
-print("Hello GitHub Project")
+def menu():
+    print("\n===== QUẢN LÝ CỬA HÀNG ĐỒ ĂN VẶT =====")
+    print("1. Thêm sản phẩm")
+    print("2. Xem danh sách sản phẩm")
+    print("3. Thoát")
+
+def main():
+    while True:
+        menu()
+        choice = input("Chọn chức năng: ")
+
+        if choice == "1":
+            print("👉 Thêm sản phẩm")
+        elif choice == "2":
+            print("👉 Danh sách sản phẩm")
+        elif choice == "3":
+            print("Thoát chương trình")
+            break
+        else:
+            print("❌ Lựa chọn không hợp lệ")
+
+if __name__ == "__main__":
+    main()
