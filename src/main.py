6854faa6b2913d1a3a03bf7fe02710bf40f2949b
TranDuc-prg <tduc45310@gmail.com>
2026-04-12 00:14:08 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,19 @@
+products = []
+
+def add_product():
+    name = input("Nhập tên sản phẩm: ")
+    price = input("Nhập giá: ")
+    products.append({"name": name, "price": price})
+    print("✔ Đã thêm sản phẩm!")
+
+def show_products():
+    if not products:
+        print("❌ Chưa có sản phẩm")
+    else:
+        print("\n--- DANH SÁCH SẢN PHẨM ---")
+        for i, p in enumerate(products, 1):
+            print(f"{i}. {p['name']} - {p['price']}")
+
 def menu():
     print("\n===== QUẢN LÝ CỬA HÀNG ĐỒ ĂN VẶT =====")
     print("1. Thêm sản phẩm")
@@ -10,9 +26,9 @@ def main():
         choice = input("Chọn chức năng: ")
 
         if choice == "1":
-            print("👉 Thêm sản phẩm")
+            add_product()
         elif choice == "2":
-            print("👉 Danh sách sản phẩm")
+            show_products()
         elif choice == "3":
             print("Thoát chương trình")
             break
